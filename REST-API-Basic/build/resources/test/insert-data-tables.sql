a40e2666437aa8b5dd11ed62a2f4cb36fa462888
Ivan Hrachyshkin <ivanhrachyshkin@gmail.com>
2022-01-19 20:34:28 +0300

    .gitignore repaired

---
+++ REST-API-Basic/build/resources/test/insert-data-tables.sql
@@ -0,0 +1,11 @@
+INSERT INTO tag (name)
+VALUES ('firstTag');
+INSERT INTO tag (name)
+VALUES ('secondTag');
+INSERT INTO tag (name)
+VALUES ('thirdTag');
+INSERT INTO gift_certificate (name, description, price, duration, create_date, last_update_date)
+VALUES ('g1', 'd1', 1.0, 1, '2010-01-01 01:01:01.111', '2010-01-01 01:01:01.111');
+INSERT INTO gift_certificate (name, description, price, duration, create_date, last_update_date)
+VALUES ('g2', 'd2', 2.0, 2, '2010-01-01 01:01:01.111', '2010-01-01 01:01:01.111');
+
