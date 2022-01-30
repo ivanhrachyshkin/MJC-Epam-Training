905693b23c12dc9afaeb81176fc7af73bfb038bb
Ivan Hrachyshkin <ivanhrachyshkin@gmail.com>
2022-01-30 17:24:59 +0300

    added javadoc

--- REST-API-Basic/controller/src/main/java/com/epam/esm/controller/GiftCertificateController.java
+++ REST-API-Basic/controller/src/main/java/com/epam/esm/controller/GiftCertificateController.java
@@ -1,14 +1,17 @@
 package com.epam.esm.controller;
 
 
-import com.epam.esm.service.dto.GiftCertificateDto;
 import com.epam.esm.service.GiftCertificateService;
+import com.epam.esm.service.dto.GiftCertificateDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * The intention of controller - handling of the /gift resource.
+ */
 @RestController
 @RequestMapping(value = "/gift")
 public class GiftCertificateController {
@@ -19,12 +22,28 @@ public class GiftCertificateController {
         this.giftCertificateService = giftCertificateService;
     }
 
+    /**
+     * The intention of mapping - handling of the creation operation.
+     *
+     * @param giftCertificateDto the request body representation of the gift certificate.
+     * @return the response body representation of the gift certificate.
+     */
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public GiftCertificateDto create(@RequestBody GiftCertificateDto giftCertificateDto) {
         return giftCertificateService.create(giftCertificateDto);
     }
 
+    /**
+     * The intention of mapping - handling of the read operation.
+     *
+     * @param tag the request param representation of the tag's name.
+     * @param name the request param representation of the gift certificate's name.
+     * @param description the request param representation of the gift certificate's description.
+     * @param dateSortDirection the request param representation of sort direction by date.
+     * @param nameSortDirection the request param representation of sort direction by name.
+     * @return the response body representation of the gift certificates.
+     */
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public List<GiftCertificateDto> readAll(@RequestParam(required = false) final String tag,
@@ -35,12 +54,25 @@ public class GiftCertificateController {
         return giftCertificateService.readAll(tag, name, description, dateSortDirection, nameSortDirection);
     }
 
+    /**
+     * The intention of mapping - handling of the read one operation.
+     *
+     * @param id the request path variable representation of the gift certificate's id.
+     * @return the response body representation of the gift certificate.
+     */
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public GiftCertificateDto readOne(@PathVariable final int id) {
         return giftCertificateService.readOne(id);
     }
 
+    /**
+     * The intention of mapping - handling of the update operation.
+     *
+     * @param id the request path variable representation of the gift certificate's id.
+     * @param giftCertificateDto the request body representation of the gift certificate.
+     * @return the response body representation of the gift certificate.
+     */
     @PatchMapping(value = "/{id}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
@@ -50,6 +82,10 @@ public class GiftCertificateController {
         return giftCertificateService.update(giftCertificateDto);
     }
 
+    /**
+     * The intention of mapping - handling of the update operation.
+     * @param id the request path variable representation of the gift certificate's id.
+     */
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable int id) {
--- REST-API-Basic/controller/src/main/java/com/epam/esm/controller/TagController.java
+++ REST-API-Basic/controller/src/main/java/com/epam/esm/controller/TagController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * The intention of controller - handling of the /tag resource.
+ */
 @RestController
 @RequestMapping(value = "/tag")
 public class TagController {
@@ -18,24 +21,45 @@ public class TagController {
         this.tagService = tagService;
     }
 
+    /**
+     * The intention of mapping - handling of the creation operation.
+     *
+     * @param tagDto the request body representation of the tag.
+     * @return the response body representation of the tag.
+     */
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public TagDto create(@RequestBody final TagDto tagDto) {
         return tagService.create(tagDto);
     }
 
+    /**
+     * The intention of mapping - handling of the read operation.
+     *
+     * @return the response body representation of the tags.
+     */
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public List<TagDto> readAll() {
         return tagService.readAll();
     }
 
+    /**
+     * The intention of mapping - handling of the read one operation.
+     *
+     * @param id the request path variable representation of the tags's id.
+     * @return
+     */
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     public TagDto readOne(@PathVariable final int id) {
         return tagService.readOne(id);
     }
 
+    /**
+     * The intention of mapping - handling of the delete operation.
+     * @param id the request path variable representation of the tag's id.
+     */
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable final int id) {
